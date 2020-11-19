552ff4407794141b637ebf2fb2baa4fb1e3a4188
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-11-19 23:43:34 +0530

    Added password validation in TextScreen

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -3,18 +3,22 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const TextScreen = () => {
 
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <View>
-            <Text>Enter name : </Text>
+            <Text>Enter Password : </Text>
             <TextInput
                 style={styles.input}
                 autoCapitalize="none"
                 autoCorrect={false}
-                value={name}
-                onChangeText={newValue => setName(newValue)} />
-            <Text>{name}</Text>
+                value={password}
+                onChangeText={newValue => setPassword(newValue)} />
+            {
+                password.length <= 5
+                ?<Text>Password must be longer than 5 characters</Text>
+                :null
+            }
         </View>
     );
 }
