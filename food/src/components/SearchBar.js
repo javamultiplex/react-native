0265df1171f65cd089910aef34aca945125dfa29
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-11-22 16:19:56 +0530

    Added state in Search Screen

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return <View style={styles.backgroundStyle}>
         <Feather name="search" style={styles.iconStyle} />
-        <TextInput style={styles.inputStyle} placeholder="Search" />
+        <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle}
+            placeholder="Search"
+            value={term}
+            onChangeText={newTerm => onTermChange(newTerm)}
+            onEndEditing={() => onTermSubmit()} />
     </View>
 }
 
 
 const styles = StyleSheet.create({
     backgroundStyle: {
-        marginTop:15,
+        marginTop: 15,
         backgroundColor: '#D3D3D3',
         height: 50,
         borderRadius: 5,
