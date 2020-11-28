1c42fa3f983060a1b8ef64442e5a7197d3cc1642
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-11-28 15:35:10 +0530

    Started working on new screen for restaurant app

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
@@ -11,7 +11,11 @@ const ResultsList = ({ title, results }) => {
                 showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={(result) => result.id}
-                renderItem={({ item }) => <ResultsDetail result={item} />}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+                        <ResultsDetail result={item} />
+                    </TouchableOpacity>
+                )}
             />
         </View>
     );
@@ -25,7 +29,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         marginLeft: 15,
-        marginBottom:5
+        marginBottom: 5
     }
 });
 
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import useResults from '../hooks/useResults';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
@@ -23,13 +23,16 @@ const SearchScreen = () => {
             <ScrollView>
                 <ResultsList
                     results={filterResultsByPrice('$')}
-                    title="Cost Effective" />
+                    title="Cost Effective"
+                    navigation={navigation} />
                 <ResultsList
                     results={filterResultsByPrice('$$')}
-                    title="Bit Pricier" />
+                    title="Bit Pricier"
+                    navigation={navigation} />
                 <ResultsList
                     results={filterResultsByPrice('$$$')}
-                    title="Big Spender" />
+                    title="Big Spender"
+                    navigation={navigation} />
             </ScrollView>
         </>
     );
