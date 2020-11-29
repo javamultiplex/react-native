44cfb22536d45345e126ccd22541e886cf0dfdb3
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-11-29 19:57:57 +0530

    Added form in create screen

---
+++ blog/src/screens/CreateScreen.js
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = ({ navigation }) => {
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+	const { addBlogPost } = useContext(Context);
+	return (
+		<View>
+			<Text style={styles.label}>Enter Title:</Text>
+			<TextInput
+				style={styles.input}
+				value={title}
+				onChangeText={title => setTitle(title)}
+			/>
+			<Text style={styles.label}>Enter Content:</Text>
+			<TextInput
+				style={styles.input}
+				value={content}
+				onChangeText={content => setContent(content)}
+			/>
+			<Button
+				title='Add Blog Post'
+				onPress={() => addBlogPost(title, content)}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	label: {
+		fontSize: 20,
+		marginLeft: 5,
+	},
+	input: {
+		fontSize: 18,
+		borderWidth: 2,
+		borderColor: 'black',
+		padding: 5,
+		margin: 5,
+	},
+});
+
+export default CreateScreen;
