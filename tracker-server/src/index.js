e374bc09e9cc57c55381fda46ea9d980ba0e037e
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-12-01 23:29:48 +0530

    Connected to mongo DB

--- tracker-server/src/index.js
+++ tracker-server/src/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const mongoUri =
+	'mongodb+srv://admin:admin@cluster0.3foxl.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+	console.log('Connected successfully');
+});
+
+mongoose.connection.on('error', err => {
+	console.error('Error in connection', err);
+});
 
 app.get('/', (req, res) => {
 	res.send('Hi There!!!');
