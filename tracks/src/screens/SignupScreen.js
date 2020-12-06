2cca3bba345b4ec383b3815ed9deb36734e0690a
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-12-06 11:06:10 +0530

    Setup app navigation

---
+++ tracks/src/screens/SignupScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const SignupScreen = ({ navigation }) => {
+	return (
+		<>
+			<Text style={{ fontSize: 48 }}>SignupScreen</Text>
+			<Button
+				title='Go to Signin Screen'
+				onPress={() => navigation.navigate('Signin')}
+			/>
+			<Button
+				title='Go to Main Flow'
+				onPress={() => navigation.navigate('mainFlow')}
+			/>
+		</>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
