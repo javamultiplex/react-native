5dec71d185e074c56e81e3f023519fe7dc9a92ad
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-01-03 16:46:06 +0530

    Added track list screen

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { Context as TrackContext } from '../contexts/TrackContext';
+import { NavigationEvents } from 'react-navigation';
+import { Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 const TrackListScreen = ({ navigation }) => {
+	const { state, fetchTracks } = useContext(TrackContext);
 	return (
 		<>
-			<Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-			<Button
-				title='Go to Track Detail Screen'
-				onPress={() => navigation.navigate('TrackDetail')}
+			<NavigationEvents onWillFocus={fetchTracks} />
+			<Text style={{ fontSize: 47 }}>TrackListScreen</Text>
+			<FlatList
+				data={state}
+				keyExtractor={item => item._id}
+				renderItem={({ item }) => {
+					return (
+						<TouchableOpacity
+							onPress={() =>
+								navigation.navigate('TrackDetail', { _id: item._id })
+							}
+						>
+							<ListItem>
+								<ListItem.Content>
+									<ListItem.Title>{item.name}</ListItem.Title>
+								</ListItem.Content>
+								<ListItem.Chevron />
+							</ListItem>
+						</TouchableOpacity>
+					);
+				}}
 			/>
 		</>
 	);
